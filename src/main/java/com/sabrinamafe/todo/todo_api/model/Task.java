37bafc41de0549a02811e78cc8e06c1d10a78a26
Sabrina Mafé <58732017+SabrinaMafe@users.noreply.github.com>
2025-07-10 16:18:54 -0300

    add Task model, repository, and service with Lombok integration

---
+++ src/main/java/com/sabrinamafe/todo/todo_api/model/Task.java
@@ -0,0 +1,28 @@
+package com.sabrinamafe.todo.todo_api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private boolean completed;
+
+
+}
---
+++ src/main/java/com/sabrinamafe/todo/todo_api/service/TaskService.java
@@ -0,0 +1,40 @@
+package com.sabrinamafe.todo.todo_api.service;
+import com.sabrinamafe.todo.todo_api.model.Task;
+import com.sabrinamafe.todo.todo_api.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Optional<Task> updateTask(Long id, Task taskDetails) {
+        return taskRepository.findById(id).map(task -> {
+            task.setTitle(taskDetails.getTitle());
+            task.setDescription(taskDetails.getDescription());
+            task.setCompleted(taskDetails.isCompleted());
+            return taskRepository.save(task);
+        });
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
