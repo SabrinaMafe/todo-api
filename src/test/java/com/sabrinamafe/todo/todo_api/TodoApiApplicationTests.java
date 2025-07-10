8def61f768a0f8f17d0644549d19231d03300579
Sabrina Mafé <58732017+SabrinaMafe@users.noreply.github.com>
2025-07-10 15:56:32 -0300

    Initial commit: project structure generated from Spring (Initializr)

---
+++ src/test/java/com/sabrinamafe/todo/todo_api/TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabrinamafe.todo.todo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
